08a8f3ee8d11807299338d24b3a897003ab0d7df
Nandini Anaparti <nanapart@student.ubc.ca>
2026-09-24 09:55:32 -0700

    Revert "Anjana/api layer"

--- app/main.py
+++ app/main.py
@@ -1,19 +1,13 @@
 from fastapi import FastAPI
 
-from app.api.routes.restaurants import router as restaurants_router
-
 app = FastAPI(
     title="The Dispatchers - Food Delivery App",
     version="0.1.0"
 )
 
-app.include_router(restaurants_router)
-
-
 @app.get("/")
 def root():
     return {"message": "The Dispatchers is running"}
-
 
 @app.get("/health")
 def health_check():
